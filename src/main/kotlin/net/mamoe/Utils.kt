4f5cd34d09bcb71ff416b04f6bb36fe55f434fae
jiahua.liu <n@mamoe.net>
2021-06-24 17:10:03 +0800

    more clients

--- src/main/kotlin/net/mamoe/Utils.kt
+++ src/main/kotlin/net/mamoe/Utils.kt
@@ -2,6 +2,7 @@
 @file:OptIn(ExperimentalContracts::class)
 package net.mamoe
 
+import io.ktor.http.*
 import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlinx.serialization.Serializable
 import org.jsoup.Connection
@@ -11,15 +12,16 @@ import java.net.*
 import java.util.concurrent.SynchronousQueue
 import java.util.concurrent.ThreadPoolExecutor
 import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicInteger
 import kotlin.contracts.ExperimentalContracts
 import kotlin.contracts.InvocationKind
 import kotlin.contracts.contract
 
-class Ksoup(
+open class Ksoup(
 
 ){
-    private val intrinsics = mutableListOf<IntrinsicHandler>()
-    private val responseHandler = mutableListOf<ResponseHandler>()
+    internal val intrinsics = mutableListOf<IntrinsicHandler>()
+    internal val responseHandler = mutableListOf<ResponseHandler>()
 
     fun interface IntrinsicHandler {
         operator fun Ksoup.invoke(p2: Connection)
@@ -138,8 +140,69 @@ class Ksoup(
         }
         return resp.applyHandlers()
     }
+}
 
 
+/**
+ * 简单模拟为Chrome访问
+ * 增加了userAgents 自动记忆cookies
+ */
+open class MockChromeClient : Ksoup() {
+    open val cookies = mutableMapOf<String,String>()
+
+    init {
+        addResponseHandler{
+            cookies.putAll(it.cookies())
+        }
+        addIntrinsic{
+            it.cookies(cookies)
+            it.userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36")
+            it.header("sec-ch-ua","\" Not;A Brand\";v=\"99\", \"Google Chrome\";v=\"91\", \"Chromium\";v=\"91\"")
+            it.header("sec-ch-ua-mobile:","?0")
+            it.header("Sec-Fetch-Dest","empty")
+            it.header("Sec-Fetch-Mode","cors")
+            it.header("Sec-Fetch-Site","same-origin")
+        }
+    }
+}
 
 
+/**
+ * Steam特制的Client, 增加了steam独有的ajax参数
+ */
+open class SteamClient: MockChromeClient(){
+    internal val ajaxCounter = AtomicInteger(1)
+
+    @PublishedApi
+    internal fun nextAjaxCount():String = "" + ajaxCounter.addAndGet(1)
+
+    suspend inline fun ajax(url: String, block: Connection.() -> Unit = {}): Connection.Response {
+        contract { callsInPlace(block, InvocationKind.EXACTLY_ONCE) }
+        return Jsoup.connect(url).apply { method(POST) }.apply {
+            data("count",nextAjaxCount())
+            header("X-Requested-With","XMLHttpRequest")
+            header("X-Prototype-Version","1.7")
+            header("accept","text/javascript, text/html, application/xml, text/xml, */*")
+            header("Accept-Encoding","gzip, deflate, br")
+            header("Accept-Language","zh-CN,zh;q=0.9")
+            header("Cache-Control","no-cache")
+        }.applyIntrinsics().apply(block).executeSuspend(threadPool)
+    }
+}
+
+
+/**
+ * Steam Store 特制的Client, 自动添加headers
+ */
+open class SteamStoreClient: SteamClient(){
+
+    val referer = "https://store.steampowered.com"
+
+    init {
+        addIntrinsic{
+            it.header("Host","store.steampowered.com")
+            it.header("Origin","https://store.steampowered.com")
+            it.header("Referer",referer)
+        }
+    }
 }
